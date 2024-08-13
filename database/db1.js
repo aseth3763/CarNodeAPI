690f09a3a8762830324163c0b4f22d46ce241746
Akshay Seth <aseth3763@gmail.com>
2024-08-13 11:54:18 +0530

    Second commit

--- database/db1.js
+++ database/db1.js
@@ -1,24 +1,32 @@
 const mongoose = require("mongoose");
-const mongoURL = "mongodb://localhost:27017/Car_database";
+require("dotenv").config();
 
-mongoose.connect(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const MONGO_URL = process.env.db_url_local
+const MONGO_URL = process.env.DB_URL; // Using uppercase for environment variables
+console.log(MONGO_URL);
+
+if (!MONGO_URL) {
+  console.error(
+    "MongoDB connection string is missing. Please set the DB_URL environment variable."
+  );
+  process.exit(1); // Exit process if environment variable is not set
+}
+
+mongoose.connect(MONGO_URL);
 
 const db = mongoose.connection;
 
-db.on("connected", ()=>{
-    console.log("Connected get successfully");
-})
+// Handle connection events
+db.on("connected", () => {
+  console.log("Connected successfully to MongoDB.");
+});
 
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
 
-db.on("error", (error)=>{
-    console.log("Connected get successfully" , error);
-})
-
-db.on("disconnected", ()=>{
-    console.log("Not disconnected");
-})
+db.on("disconnected", () => {
+  console.log("Disconnected from MongoDB.");
+});
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 3500;
+const port = process.env.port || 3200;
 
 const db = require("./database/db1");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
+require("dotenv").config()
 
 const CarRouter = require("./router/carRouter")
 app.use("/Car_data",CarRouter)
 
 
 app.listen(port, () => {
-  console.log("connection listening on port 3500");
+  console.log(`connection listening on port ${port}`);
 });
